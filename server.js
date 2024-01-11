90be6c1f8d70570fe401ff3cf875800c0c6418fe
PremNodeDeveloper <premprakash1519@gmail.com>
2024-01-11 22:24:41 +0530

    Revert "Revert "created server with first commit""
    This reverts commit e024c9a44b981a2e9ed3989ee8f57c4192d25fa1.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.end("you are at home page");
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at port ${port}`);
+});
+
+module.exports = app;
